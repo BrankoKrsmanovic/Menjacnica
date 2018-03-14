0d5ed86f912ebe6cd6bab395c9bd29448ac360b6
BrankoKrsmanovic <bkrsmanovic97@gmail.com>
2018-03-14 15:40:11 +0100

    Dodate metode equals, hashCode i toString u klase Valute i Kurs.

--- menjacniaGit/src/valuta/Valuta.java
+++ menjacniaGit/src/valuta/Valuta.java
@@ -41,4 +41,53 @@ public class Valuta {
 		this.kurs = kurs;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceno == null) ? 0 : skraceno.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (kurs == null) {
+			if (other.kurs != null)
+				return false;
+		} else if (!kurs.equals(other.kurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceno == null) {
+			if (other.skraceno != null)
+				return false;
+		} else if (!skraceno.equals(other.skraceno))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceno=" + skraceno + ", datum=" + datum + ", kurs=" + kurs + "]";
+	}
+
+	
 }
