a2b57da587828edc7309fe4179d1c07c2da2869c
BrankoKrsmanovic <bkrsmanovic97@gmail.com>
2018-03-14 15:55:31 +0100

    Napravljena klasa Menjacnica.

---
+++ menjacniaGit/src/menjacnica/Menjacnica.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnicaInterfejs.IMenjacnica;
+import valuta.Kurs;
+
+public class Menjacnica implements IMenjacnica{
+
+	@Override
+	public void dodajKurs(String naziv, GregorianCalendar datum, Kurs kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
