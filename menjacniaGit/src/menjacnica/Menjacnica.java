f807b942ba5b8201adb22a11518ffb828907b23b
BrankoKrsmanovic <bkrsmanovic97@gmail.com>
2018-03-14 18:57:52 +0100

    Uradjena specifikacija klase dodoajKurs u okviru master grane.

--- menjacniaGit/src/menjacnica/Menjacnica.java
+++ menjacniaGit/src/menjacnica/Menjacnica.java
@@ -1,16 +1,40 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicaInterfejs.IMenjacnica;
 import valuta.Kurs;
+import valuta.Valuta;
 
 public class Menjacnica implements IMenjacnica{
 
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(String naziv, GregorianCalendar datum, Kurs kurs) {
-		// TODO Auto-generated method stub
+
+		boolean ubacen = false;
 		
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNaziv().equals(naziv) && valute.get(i).getDatum().equals(datum)) {
+				valute.get(i).setKurs(kurs);
+				ubacen = true;
+			}
+			else {
+				Valuta pom = new Valuta();
+				pom.setNaziv(naziv);
+				pom.setSkraceno(naziv.substring(0,3));
+				pom.setDatum(datum);
+				pom.setKurs(kurs);
+				
+				valute.add(pom);
+				ubacen = true;
+			}
+		}
+		
+		if(!ubacen)
+			throw new RuntimeException("ERROR: Kurs nije ubacen!");
 	}
 
 	@Override
